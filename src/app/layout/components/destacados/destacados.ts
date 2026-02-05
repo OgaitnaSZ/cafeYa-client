7cda4def8f295ce93c0a99b2711e758bd516b132
OgaitnaSZ <zanty201212@gmail.com>
2026-02-04 22:15:44 -0300

    update /home and delete /scan component

--- src/app/layout/components/destacados/destacados.ts
+++ src/app/layout/components/destacados/destacados.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-destacados',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './destacados.html',
   styleUrl: './destacados.css',
 })
