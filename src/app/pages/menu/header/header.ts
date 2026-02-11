d7fcb48c2d675941f40de924defe171e51976a50
OgaitnaSZ <zanty201212@gmail.com>
2026-02-10 21:07:15 -0300

    fix image in product modal

--- src/app/pages/menu/header/header.ts
+++ src/app/pages/menu/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../../core/services/cart';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
