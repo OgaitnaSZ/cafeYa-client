712feec37ce53bf251acdec597b6a8c82137742f
OgaitnaSZ <zanty201212@gmail.com>
2026-02-24 18:34:54 -0300

    Add socketUrl on environment env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cafe-ya-server.vercel.app/api/'
+  apiUrl: 'https://cafe-ya-server.vercel.app/api/',
+  socketUrl: 'https://cafe-ya-server.vercel.app/'
 };
